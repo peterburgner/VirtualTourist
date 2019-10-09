1885c000cd999c859cd2457b3c7364317107edca
Peter Burgner <dev@burgner.de>
2019-10-09 00:08:49 -0700

    feat: Set and read region in UserDefaults

--- VirtualTourist/Controllers/TravelLocationsMapController.swift
+++ VirtualTourist/Controllers/TravelLocationsMapController.swift
@@ -9,24 +9,44 @@
 import UIKit
 import MapKit
 
-class TravelLocationsMapController: UIViewController {
+class TravelLocationsMapController: UIViewController, MKMapViewDelegate {
     
+    // MARK: - Variables
     @IBOutlet weak var mapView: MKMapView!
     
+    // MARK: -View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         prepareUI()
-
     }
     
     func prepareUI() {
         // Prepare map
-//        mapView.centerCoordinate = placemark[0].location!.coordinate
-//        map.addAnnotation(MKPlacemark(placemark: placemark[0]))
-//        let region = MKCoordinateRegion(center: map.centerCoordinate, latitudinalMeters: 100000, longitudinalMeters: 100000)
-//        map.setRegion(region, animated: true)
+        print("Setting default")
+//        print(UserDefaults.standard.float(forKey: "latitude"))
+//            let coordinate = CLLocationCoordinate2D(latitude: regionData["latitude"], longitude: regionData["longitude"])
+//            let region = MKCoordinateRegion(center: <#T##CLLocationCoordinate2D#>, span: <#T##MKCoordinateSpan#>)
+
+//            mapView.setRegion(coordinateRegion as! MKCoordinateRegion, animated: true)
+
     }
     
+    // MARK: -IBActions
     @IBAction func edit(_ sender: Any) {
+    }
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        print(mapView.region)
+        UserDefaults.standard.set(mapView.region.center.latitude, forKey: "latitude") // TODO: Remove, for testing only
+        let regionData = ["latitude":mapView.region.center.latitude, "longitude":mapView.region.center.longitude, "latitudeDelta": mapView.region.span.latitudeDelta, "longitudeDelta":mapView.region.span.longitudeDelta]
+        UserDefaults.standard.setValue(regionData, forKey: "regionData")
+        
+        print("Read")
+        let loadedData = UserDefaults.standard.dictionary(forKey: "regionData")
+        let location = CLLocationCoordinate2D(latitude: loadedData!["latitude"] as! CLLocationDegrees, longitude: loadedData!["longitude"] as! CLLocationDegrees)
+        let span = MKCoordinateSpan(latitudeDelta: loadedData!["latitudeDelta"] as! CLLocationDegrees, longitudeDelta: loadedData!["longitudeDelta"] as! CLLocationDegrees)
+        let region = MKCoordinateRegion(center: location, span: span)
+        print(region)
     }
 }
