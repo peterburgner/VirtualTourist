d493747cb8964b3e22ba5d4308641d7326b34f30
Peter Burgner <dev@burgner.de>
2019-10-08 21:48:38 -0700

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Peter Burgner on 10/8/19.
+//  Copyright © 2019 Peter Burgner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
