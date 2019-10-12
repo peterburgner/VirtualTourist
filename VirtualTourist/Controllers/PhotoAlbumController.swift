e32ed8412a4aaf93097883db7135b48ffc5fbf6b
Peter Burgner <dev@burgner.de>
2019-10-12 10:24:05 -0700

    refactor: Remove unnecessary background message method

--- VirtualTourist/Controllers/PhotoAlbumController.swift
+++ VirtualTourist/Controllers/PhotoAlbumController.swift
@@ -179,15 +179,11 @@ extension UICollectionView {
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         messageLabel.text = message
         messageLabel.textColor = .black
-        messageLabel.numberOfLines = 0;
-        messageLabel.textAlignment = .center;
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
         messageLabel.font = UIFont(name: "Avenir-Light", size: 18)
         messageLabel.sizeToFit()
 
-        self.backgroundView = messageLabel;
-    }
-
-    func restore() {
-        self.backgroundView = nil
+        self.backgroundView = messageLabel
     }
 }
